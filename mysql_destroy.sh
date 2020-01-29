052ae26234dda314aa75135579254c1a3cbe27b2
Thomas Pellizzari <t.pellizzari@synedra.com>
2020-01-29 05:12:29 +0100

    -

--- mysql_destroy.sh
+++ mysql_destroy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-kubectl delete deployment,svc mysql
-kubectl delete pvc mysql-pv-claim
-kubectl delete pv mysql-pv-volume
+kubectl delete deployment mysql-deployment
+# kubectl delete svc mysql
+kubectl delete pvc mysql-data-disk
+# kubectl delete pv mysql-pv-volume
