3bd72f9d7fa02fa0e66f119a6f7efc6d8f1ed094
Thomas Pellizzari <t.pellizzari@synedra.com>
2020-01-29 21:22:17 +0100

    -

--- nginx/create.sh
+++ nginx/create.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
 
 # https://8gwifi.org/docs/kube-nginx.jsp
 
 openssl req -newkey rsa:2048 -nodes -keyout nginx.key -x509 -days 365 -out nginx.crt -config openssl.cnf
+kubectl delete secret nginx-certs-keys
 kubectl create secret generic nginx-certs-keys --from-file=./nginx.crt --from-file=./nginx.key 
+rm -f nginx.key nginx.cnf
+kubectl delete configmap nginxconfigmap 
 kubectl create configmap nginxconfigmap --from-file=default.conf
 kubectl apply -f nginx-app.yaml
