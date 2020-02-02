a77acfdf2dd0fa08cb133b54f430b44e3027332f
tompelli <31741399+tompelli@users.noreply.github.com>
2020-02-02 22:04:10 +0100

    -

--- aim_app/destroy_aim_app.sh
+++ aim_app/destroy_aim_app.sh
@@ -2,5 +2,5 @@
 
 kubectl delete deployment synedra-aim-app-deployment
 kubectl delete svc synedra-aim-app
-kubectl delete pvc synedra-aim-pg-disk
+kubectl delete pvc synedra-data-node1-disk
 # kubectl delete pv mysql-pv-volume
