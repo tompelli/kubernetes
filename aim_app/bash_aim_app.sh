320461524b9e5921fb17ff8591994c36db4b97f0
Thomas Pellizzari <t.pellizzari@synedra.com>
2020-02-02 22:46:11 +0100

    -

--- aim_app/bash_aim_app.sh
+++ aim_app/bash_aim_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl exec -ti `kubectl get pods|grep -v NAME|grep aim_app|awk '{print $1}'` -- /bin/bash
+kubectl exec -ti `kubectl get pods|grep -v NAME|grep aim-app|awk '{print $1}'` -- /bin/bash
