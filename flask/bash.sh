3fd7f99f75685d4865b0568d80c6ea1f9827adf1
Thomas Pellizzari <t.pellizzari@synedra.com>
2020-02-10 20:39:58 +0100

    -

--- flask/bash.sh
+++ flask/bash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl exec -ti `kubectl get pods|grep -v NAME|grep flask|awk '{print $1}'` -- /bin/bash
+kubectl exec -ti `kubectl get pods|grep -v NAME|grep Running|grep flask|awk '{print $1}'` -- /bin/bash
