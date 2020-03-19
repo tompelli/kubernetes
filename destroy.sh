389b89febb4d9777551f8d3b37eb66ed74aa8305
tompelli <31741399+tompelli@users.noreply.github.com>
2020-03-19 06:22:59 +0100

    -

--- destroy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aim_app/destroy.sh
-aim_db/destroy.sh
-workspace/destroy.sh
-angular/destroy.sh
-flask/destroy.sh
--- synedra_aim/restart.sh
+++ synedra_aim/restart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -v
 
+../workspace/restart.sh
 ../aim_app/restart.sh
 ../aim_db/restart.sh
-../workspace/restart.sh
 ../flask/restart.sh
 ../angular/restart.sh
+../nginx/restart.sh
+../r-shiny/restart.sh
