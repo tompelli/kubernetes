66216bf59f5c808f21a4654097be3f35e929fb5c
Thomas Pellizzari <t.pellizzari@synedra.com>
2020-03-19 04:49:04 +0100

    -

--- synedra_aim/apply.sh
+++ synedra_aim/apply.sh
@@ -6,3 +6,4 @@ kubectl apply -f ../aim_db/manifest.yaml
 kubectl apply -f ../flask/manifest.yaml
 kubectl apply -f ../angular/manifest.yaml
 kubectl apply -f ../nginx/manifest.yaml
+kubectl apply -f ../r-shiny/manifest.yaml
