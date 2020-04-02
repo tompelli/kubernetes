261e982a1618341ae61538e50ab5fb069df62e37
Thomas Pellizzari <t.pellizzari@synedra.com>
2020-04-02 06:00:43 +0200

    -

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,10 @@
 alias bas='~/kubernetes/r-shiny/bash.sh'
+alias baa='~/kubernetes/angular/bash.sh'
+
 alias los='~/kubernetes/r-shiny/logs.sh -f'
+alias loa='~/kubernetes/angular/logs.sh -f'
+
 alias cdk="cd ~/kubernetes"
+alias cop="~/kubernetes/commit_push.sh"
+
+alias rel="source ~/kubernetes/aliases.sh"
